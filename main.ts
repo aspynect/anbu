15e817e6dbfac7b6fd39fbadc096ca03e6520ff2
aspynect <aspynect@gmail.com>
2026-01-26 10:46:12 -0600

    ok ummm so that check was so invalid lol

--- main.ts
+++ main.ts
@@ -57,7 +57,7 @@ async function jetMessage(event: MessageEvent) {
     {
         let trainingString = ""
         let isReply = false
-        if (msg.reply?.parent.$type === "app.bsky.feed.defs#postView") {
+        if (msg.reply?.parent) {
             isReply = true
             const parentText = msg.reply.parent.record.text
             if (typeof parentText === "string" && parentText.length >= 10) {
